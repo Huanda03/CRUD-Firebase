55a983fd01d0f65485577de744460b9b06237d85
Andrés Reyes <yoigaiden@gmail.com>
2021-01-13 08:27:01 -0600

    Creacion del proyecto

---
+++ CRUD Firebase/AlertService.swift
@@ -0,0 +1,12 @@
+//
+//  AlertService.swift
+//  CRUD Firebase
+//
+//  Created by Mac2 on 13/01/21.
+//
+
+import Foundation
+
+class AlertService {
+    
+}
---
+++ CRUD Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CRUD Firebase
+//
+//  Created by Mac2 on 13/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onAddTapped(_ sender: UIBarButtonItem) {
+    }
+}
+
